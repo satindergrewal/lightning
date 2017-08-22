a8e4ae7786629d528e90f674bbad3d7670ac5f43
jl777 <jameslee777@yahoo.com>
2017-08-22 11:44:20 +0200

    peer_htlcs.h

--- lightningd/peer_htlcs.h
+++ lightningd/peer_htlcs.h
@@ -4,37 +4,38 @@
 #include "config.h"
 #include <ccan/short_types/short_types.h>
 #include <lightningd/derive_basepoints.h>
+#include <lightningd/htlc_wire.h>
 
 /* FIXME: Define serialization primitive for this? */
 struct channel_info {
-	struct channel_config their_config;
-	struct pubkey remote_fundingkey;
-	struct basepoints theirbase;
-	struct pubkey remote_per_commit, old_remote_per_commit;
-	u32 feerate_per_kw;
+    struct channel_config their_config;
+    struct pubkey remote_fundingkey;
+    struct basepoints theirbase;
+    struct pubkey remote_per_commit, old_remote_per_commit;
+    u32 feerate_per_kw;
 };
 
 /* Get all HTLCs for a peer, to send in init message. */
 void peer_htlcs(const tal_t *ctx,
-		const struct peer *peer,
-		struct added_htlc **htlcs,
-		enum htlc_state **htlc_states,
-		struct fulfilled_htlc **fulfilled_htlcs,
-		enum side **fulfilled_sides,
-		struct failed_htlc **failed_htlcs,
-		enum side **failed_sides);
+                const struct peer *peer,
+                struct added_htlc **htlcs,
+                enum htlc_state **htlc_states,
+                struct fulfilled_htlc **fulfilled_htlcs,
+                enum side **fulfilled_sides,
+                struct failed_htlc **failed_htlcs,
+                enum side **failed_sides);
 
 int peer_sending_commitsig(struct peer *peer, const u8 *msg);
 int peer_got_commitsig(struct peer *peer, const u8 *msg);
 int peer_got_revoke(struct peer *peer, const u8 *msg);
 
 void update_per_commit_point(struct peer *peer,
-			     const struct pubkey *per_commitment_point);
+                             const struct pubkey *per_commitment_point);
 
 enum onion_type send_htlc_out(struct peer *out, u64 amount, u32 cltv,
-			      const struct sha256 *payment_hash,
-			      const u8 *onion_routing_packet,
-			      struct htlc_in *in,
-			      struct pay_command *pc,
-			      struct htlc_out **houtp);
+                              const struct sha256 *payment_hash,
+                              const u8 *onion_routing_packet,
+                              struct htlc_in *in,
+                              struct pay_command *pc,
+                              struct htlc_out **houtp);
 #endif /* LIGHTNING_LIGHTNINGD_PEER_HTLCS_H */
