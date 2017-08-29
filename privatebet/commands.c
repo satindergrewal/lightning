ce4d2421929a24df9b3f66ac4277f7c58dfcb752
jl777 <jameslee777@yahoo.com>
2017-08-29 20:53:37 +0200

    Test

--- privatebet/commands.c
+++ privatebet/commands.c
@@ -260,7 +260,7 @@ int32_t BET_channel_status(char *peerid,char *status)
     cJSON *retjson,*array,*item,*obj; int32_t i,n,retval = -1;
     if ( (retjson= chipsln_getchannels()) != 0 )
     {
-        printf("HOST.(%s) mine.(%s) channels.(%s)\n",Host_channel,LD_idstr,jprint(retjson,0));
+        printf("HOST.(%s) mine.(%s) channels.(%s)\n",Host_channel,LN_idstr,jprint(retjson,0));
         if ( (array= jarray(&n,retjson,"channels")) != 0 && n > 0 )
         {
             for (i=0; i<n; i++)
