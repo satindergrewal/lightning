a9492eb7bdd998b53c3825bef09ab81878e6942e
jl777 <jameslee777@yahoo.com>
2017-08-31 23:52:49 +0200

    Test

--- privatebet/commands.c
+++ privatebet/commands.c
@@ -178,11 +178,13 @@ cJSON *chipsln_getroute(char *idstr,uint64_t msatoshi)
 
 cJSON *chipsln_connect(char *ipaddr,uint16_t port,char *destid)
 {
-    char buf[4096],*retstr;
+    char buf[4096],*retstr; cJSON *retjson;
     sprintf(buf,"{ \"method\":\"connect\", \"id\":\"chipsln-%d\", \"params\":[\"%s\", %u, \"%s\"] }",getpid(),ipaddr,port,destid);
-    retstr = chipsln_issue(buf);
+    retjson = chipsln_issue(buf);
+    retstr = jprint(retjson,0);
     printf("(%s) -> %s\n",buf,retstr);
-    return(retstr);
+    free(retstr);
+    return(retjson);
 }
 
 cJSON *chipsln_sendpay(cJSON *routejson,bits256 rhash)
