e600554f199de2c0ae2121b83ad9ef1fd22bbfb3
jl777 <jameslee777@yahoo.com>
2017-08-31 23:14:28 +0200

    Test

--- cli/lightning-cli.c
+++ cli/lightning-cli.c
@@ -1,6 +1,7 @@
 /*
  * Helper to submit via JSON-RPC and get back response.
  */
+#ifdef newversion
 #include <ccan/err/err.h>
 #include <ccan/opt/opt.h>
 #include <ccan/read_write_all/read_write_all.h>
@@ -186,3 +187,26 @@ int main(int argc, char *argv[])
 	tal_free(ctx);
 	return 1;
 }
+#endif
+
+int cli_main(char *buffer,int32_t maxsize,int argc, char *argv[],char *cmd);
+
+int main(int argc, char *argv[])
+{
+    int32_t retval = -1,maxsize = 1000000; char *buffer = malloc(maxsize);
+    if ( buffer != 0 )
+    {
+        if ( cli_main(buffer,maxsize,argc,argv,0) == 0 )
+        {
+            printf("%s",buffer);
+            retval = 1;
+        }
+        else
+        {
+            printf("error %s",buffer);
+        }
+        free(buffer);
+    }
+    return(retval);
+}
+
