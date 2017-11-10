0385b18d15c7c7549f6be1e9799f8d7b0f7b01da
saratchandraprasadg <sarath.ginjupalli89@gmail.com>
2017-11-10 06:36:40 +0530

    Test

--- privatebet/bet.c
+++ privatebet/bet.c
@@ -408,7 +408,7 @@ void blinding_vendor(bits256 *blindings,bits256 *blindedcards,bits256 *finalcard
 {
     static bits256 *allshares;
     int32_t i,j,M,permi,permis[256]; uint8_t sharenrs[256],space[8192]; bits256 *cardshares;
-    BET_permutation(permis,numcards);
+    //BET_permutation(permis,numcards);
     
     for (i=0; i<numcards; i++)
     {
