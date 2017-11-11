de873253a2b1e02e2fa9296fee787535229a3c6c
saratchandraprasadg <sarath.ginjupalli89@gmail.com>
2017-11-11 15:59:43 +0530

    test

--- privatebet/bet.c
+++ privatebet/bet.c
@@ -431,19 +431,19 @@ void blinding_vendor(bits256 *blindings,bits256 *blindedcards,bits256 *finalcard
 		cardshares = calloc(numplayers,sizeof(bits256));
         if ( allshares == 0 )
             allshares = calloc(numplayers,sizeof(bits256) * numplayers * numcards);
-		printf("\nplayer:%d",playerid);
+		//printf("\nplayer:%d",playerid);
         for (i=0; i<numcards; i++)
         {
-        	printf("\ncard:%d",i);
+        	//printf("\ncard:%d",i);
             gfshare_calc_shares(cardshares[0].bytes,blindings[i].bytes,sizeof(bits256),sizeof(bits256),M,numplayers,sharenrs,space,sizeof(space));
             // create combined allshares
             for (j=0; j<numplayers; j++) {
                 allshares[j*numplayers*numcards + (i*numplayers + playerid)] = cardshares[j];
-				printf("\nshare:%d\n",j);
+				/*printf("\nshare:%d\n",j);
 				for(k=0;k<32;k++)
 				{
 					printf("%d ",allshares[j*numplayers*numcards + (i*numplayers + playerid)].bytes[k]);
-				}
+				}*/
 			}
 			
         }
@@ -469,15 +469,15 @@ bits256 player_decode(int32_t playerid,int32_t cardID,int numplayers,struct pair
 
 	recover=calloc(1,sizeof(bits256));
 	cardshares = calloc(numplayers,sizeof(bits256));
-		printf("\nPlayer:%d:card:%d",playerid,cardID);
+		//printf("\nPlayer:%d:card:%d",playerid,cardID);
 			for (j=0; j<numplayers; j++) 
 			{
 				cardshares[j]=allshares[j*numplayers*numcards + (cardID*numplayers + playerid)];
-        		printf("\nshare:%d\n",j);
+        	/*	printf("\nshare:%d\n",j);
 				for(k=0;k<32;k++)
 				{
 					printf("%d ",cardshares[j].bytes[k]);
-				}
+				}*/
 			}
 	
 	M=(numplayers/2)+1;
@@ -501,7 +501,7 @@ bits256 player_decode(int32_t playerid,int32_t cardID,int numplayers,struct pair
 	}
     
    
-	refval = fmul_donna(blindedcard,crecip_donna(blindingval));
+	refval = fmul_donna(blindedcard,crecip_donna(*recover));
 	#if 0  
 	for (i=0; i<numcards; i++)
     {
