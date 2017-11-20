783ba9cdb6b7712aa9b8eb08c4daaf8b7b63e232
sg777 <sarath.ginjupalli89@gmail.com>
2017-11-20 14:33:21 +0530

    Test

--- privatebet/bet.c
+++ privatebet/bet.c
@@ -794,16 +794,8 @@ void sg777_players_init(int32_t numplayers,int32_t numcards,bits256 deckid)
 			 
 			 temp=curve25519_keypair(&temp1);
 
-			 printf("\nThe first product is:\n");
-			 for(i=0;i<sizeof(temp);i++){
-				printf("%02x ",temp.bytes[i]);
-			 }
 
-			 printf("\nThe second product is:\n");
-			 for(i=0;i<sizeof(temp1);i++){
-				printf("%02x ",temp1.bytes[i]);
-			 }
-			 temp1=curve25519(temp,curve25519_basepoint9());
+			temp1=curve25519(temp1,crecip_donna(curve25519_basepoint9()));
 			 printf("\nThe first product is:\n");
 			 for(i=0;i<sizeof(temp);i++){
 				printf("%02x ",temp.bytes[i]);
