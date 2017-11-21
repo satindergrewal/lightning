d299cce85a0ab562893964e70f196f988c605e3c
sg777 <sarath.ginjupalli89@gmail.com>
2017-11-21 09:40:45 +0530

    Test

--- privatebet/bet.c
+++ privatebet/bet.c
@@ -697,6 +697,8 @@ void sg777_players_init(int32_t numplayers,int32_t numcards,bits256 deckid)
     int32_t i,j,k,playerid,errs,playererrs,good,bad,permis[CARDS777_MAXPLAYERS][CARDS777_MAXCARDS]; bits256 playerprivs[CARDS777_MAXPLAYERS][CARDS777_MAXCARDS],playercards[CARDS777_MAXPLAYERS][CARDS777_MAXCARDS]; char str[65];
 	struct pair256 keys[CARDS777_MAXPLAYERS],b_key;
 	bits256 temp,decoded256,basepoint,cardprods[CARDS777_MAXPLAYERS][CARDS777_MAXCARDS],finalcards[CARDS777_MAXPLAYERS][CARDS777_MAXCARDS],blindingvals[CARDS777_MAXPLAYERS][CARDS777_MAXCARDS],blindedcards[CARDS777_MAXPLAYERS][CARDS777_MAXCARDS];
+	dekgen_vendor_perm(numcards);
+	blinding_vendor_perm(numcards);
 	
 	for (playerid=0; playerid<numplayers; playerid++) {
 		keys[playerid]=deckgen_player(playerprivs[playerid],playercards[playerid],permis[playerid],numcards);
