8c68054554bec0b01c12aaf1348fb964e789ac73
sg777 <sarath.ginjupalli89@gmail.com>
2017-11-21 08:55:38 +0530

    Test

--- privatebet/bet.c
+++ privatebet/bet.c
@@ -344,6 +344,7 @@ int main(int argc,const char *argv[])
             OS_randombytes((uint8_t *)&numplayers,sizeof(numplayers));
             range = (range % CARDS777_MAXCARDS) + 1;
             numplayers = (numplayers % (CARDS777_MAXPLAYERS-1)) + 2;
+			printf("\nnumplayers=%d, numcards=%d\n",numplayers,range);
 			players_init(numplayers,range,rand256(0));
             continue;
             for (i=0; i<numplayers; i++)
