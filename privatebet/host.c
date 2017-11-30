4d08e56eca50ef58c60d5c386a11cb15bdc37b6c
sg777 <sarath.ginjupalli89@gmail.com>
2017-11-30 11:25:08 +0530

    test

--- privatebet/host.c
+++ privatebet/host.c
@@ -371,6 +371,7 @@ void* BET_dcv(void *_ptr)
 	  int bytes=nn_recv(pullsock,&buf,NN_MSG,0);
 	  printf("\nNumber of bytes received:%d",bytes);
 	  playerInfo=cJSON_Parse(buf);
+	  cJSON_Print(playerInfo);
 	  if(is_cJSON_Object(playerInfo)==0){
 			playercards=cJSON_GetObjectItem(playerInfo,"playercards");
 			if(is_cJSON_Array(playercards)==0){
