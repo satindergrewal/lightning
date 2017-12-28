f0442ca7621f4396f9c802f03e3e9f404c08022a
sg777 <sarath.ginjupalli89@gmail.com>
2017-12-28 11:48:37 +0530

    test

--- privatebet/client.c
+++ privatebet/client.c
@@ -553,6 +553,7 @@ void* BET_clientplayer(void * _ptr)
 						}
 					}
 					else if(0==strcmp(cJSON_str(cJSON_GetObjectItem(gameInfo,"messageid")),"request_share")){
+						printf("\n%s:%d",__FUNCTION__,__LINE__);
 						BET_give_share(gameInfo,bet,public_key_b,key);
 					}
 				}
