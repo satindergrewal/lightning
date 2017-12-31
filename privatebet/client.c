726b7b6f32a48e4fa30728b987461616199968e7
sg777 <sarath.ginjupalli89@gmail.com>
2017-12-31 08:57:45 +0530

    test

--- privatebet/client.c
+++ privatebet/client.c
@@ -457,7 +457,7 @@ void BET_give_share(cJSON *shareInfo,struct privatebet_info *bet,bits256 bvv_pub
 	forPlayerID=jint(shareInfo,"forPlayerID");
 	cJSON_Print(shareInfo);
 	if((ofPlayerID==bet->myplayerid)&&(forPlayerID!=bet->myplayerid)){
-        temp=g_shares[ofPlayerID*bet->numplayers*bet->range + (ofCardID*bet->numplayers + ofPlayerID)];
+        temp=g_shares[forPlayerID*bet->numplayers*bet->range + (ofCardID*bet->numplayers + forPlayerID)];
         recvlen = sizeof(temp);
 		printf("\n%s:%d:t_id:%d:player_id:%d:%s",__FUNCTION__,__LINE__,pthread_self(),ofPlayerID,enc_share_str(enc_str,temp));	
         if ( (ptr= BET_decrypt(decipher,sizeof(decipher),bvv_public_key,player_key.priv,temp.bytes,&recvlen)) == 0 )
