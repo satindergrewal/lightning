0b2c007aadf656be2f4e9e5fe6d28b6bd9d199b7
sg777 <sarath.ginjupalli89@gmail.com>
2018-01-01 11:32:16 +0530

    test

--- privatebet/client.c
+++ privatebet/client.c
@@ -575,6 +575,7 @@ void* BET_clientplayer(void * _ptr)
 						#if 1
 					   for(int i=0;i<numcards;i++){
         				    decoded256 = t_sg777_player_decode(bet,i,numplayers,key,public_key_b,blindedcards[bet->myplayerid][i],cardprods[bet->myplayerid],playerprivs,numcards);
+							printf("\n\n");
             	            if ( bits256_nonz(decoded256) == 0 )
                 				errs++;
             				else
