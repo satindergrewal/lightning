4c7da2849baffa37467a02ef4a42200f8c8229d1
sg777 <sarath.ginjupalli89@gmail.com>
2018-04-11 19:20:12 +0530

    cleaning the code

--- privatebet/bet.c
+++ privatebet/bet.c
@@ -615,7 +615,7 @@ struct pair256 deckgen_player(bits256 *playerprivs,bits256 *playercards,int32_t 
     BET_permutation(permis,numcards);
     for (i=0; i<numcards; i++)
     {
-        playerprivs[i] = randcards[permis[i]].priv;
+        playerprivs[i] = randcards[i].priv; //permis[i]
         playercards[i]=curve25519(playerprivs[i],key.prod);
     }
     return(key);
