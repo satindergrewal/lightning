af0f07c2575d2c96e435f54c59684b47fc62987b
Satinder Grewal <grewal@lavabit.com>
2020-12-21 00:25:44 +1300

    Revert "debug"
    This reverts commit c4440ac433f7aaafb33edbe9c9a54220ae86fea8.

--- lightningd/invoice.c
+++ lightningd/invoice.c
@@ -1661,9 +1661,9 @@ static struct command_result *json_invoice_count(struct command *cmd,
 	response = json_stream_success(cmd);
 	invoice_count=wallet_invoice_count(cmd->ld->wallet);
 	
-	// json_object_start(response, NULL);
+	json_object_start(response, NULL);
 	json_add_num(response,"invoice count",invoice_count);
-	// json_object_end(response);
+	json_object_end(response);
 	return command_success(cmd, response);
 }
 
