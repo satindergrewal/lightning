8e45a0e84c3cc86926bc7286ea8e440cea581cc1
satindergrewal <grewal@lavabit.com>
2021-02-20 18:41:50 +1300

    test

--- wallet/wallet.h
+++ wallet/wallet.h
@@ -1472,6 +1472,6 @@ enum offer_status wallet_offer_disable(struct wallet *w,
 void wallet_offer_mark_used(struct db *db, const struct sha256 *offer_id)
 	NO_NULL_ARGS;
 
-// int wallet_invoice_count(struct wallet *wallet);
+int wallet_invoice_count(struct wallet *wallet);
 
 #endif /* LIGHTNING_WALLET_WALLET_H */
